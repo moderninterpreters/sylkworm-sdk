8cc00fe7edde0451dc45268c162ebff1bfd07a75
Arnold Noronha <arnold@tdrhq.com>
2020-10-01 10:31:19 -0700

    Initial option parser
    Differential Revision: https://phabricator.tdrhq.com/D2432

--- lib/src/main/kotlin/io/silkwrm/sdk/Recorder.kt
+++ lib/src/main/kotlin/io/silkwrm/sdk/Recorder.kt
@@ -1,10 +1,20 @@
 package io.silkwrm.sdk
 
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.Options
+
 class Recorder {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            System.out.println("hello")
+            var options = Options()
+            options.addOption("d",  "dir",true, "Directory with screenshots")
+            var parser = DefaultParser()
+            var cli = parser.parse(options, args)
+
+            var dir = cli.getOptionValue('d')
+
+            System.out.println("hello " + dir)
         }
     }
 }
