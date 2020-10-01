bfaf8be7df5195206625f2df218ead9dcad69a6c
Arnold Noronha <arnold@tdrhq.com>
2020-10-01 13:35:19 -0700

    Hashing of files
    Differential Revision: https://phabricator.tdrhq.com/D2438

--- lib/src/main/kotlin/io/silkwrm/sdk/Recorder.kt
+++ lib/src/main/kotlin/io/silkwrm/sdk/Recorder.kt
@@ -4,9 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.jackson.objectBody
 import com.github.kittinunf.fuel.jackson.jacksonDeserializerOf
+import com.google.common.hash.Hashing
+import com.google.common.io.Files
 import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.Options
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.security.MessageDigest
 
 data class ImageUploadResponse(var imageId: String = "",
                                var uploadUrl:String = "")
@@ -14,11 +19,16 @@ data class ImageUploadResponse(var imageId: String = "",
 data class Result<T>(var result: Boolean = false, var response: T? = null)
 
 class Recorder {
-    fun getAllImages(dir: String): List<File> {
+    fun getAllImages(dir: String) = run {
         var dirFile = File(dir)
-        return dirFile.listFiles().toList()
+        dirFile.listFiles().toList()
                 .filter { x -> x.absolutePath.endsWith(".png") }
     }
+
+    fun getDigest(file: File) = run {
+        Files.asByteSource(file).hash(Hashing.sha256()).toString()
+    }
+
     private fun run(args: Array<String>) {
         var options = Options()
         options.addOption("d", "dir", true, "Directory with screenshots")
@@ -27,16 +37,20 @@ class Recorder {
 
         var dir = cli.getOptionValue('d')
 
+        val md = MessageDigest.getInstance("MD5")
 
         val mapper = ObjectMapper()
         var allImages = getAllImages(dir);
         for (file in allImages) {
             System.out.println("Uploading file: " + file)
+            val hash = getDigest(file)
+            System.out.println("Got digest: " + hash)
             val result : Result<ImageUploadResponse>  =
                 Fuel.post("https://silkwrm.tdrhq.com/api/prepare-upload", listOf("name" to "foo", "hash" to "blah"))
-                  .responseObject<Result<ImageUploadResponse>>(jacksonDeserializerOf(mapper)).third.get()
+                .responseObject<Result<ImageUploadResponse>>(jacksonDeserializerOf(mapper)).third.get()
 
-            System.out.println("Got upload url: " + result.response?.uploadUrl)
+            val response = result.response!!
+            System.out.println("Got upload url: " + response.uploadUrl)
         }
 
 
