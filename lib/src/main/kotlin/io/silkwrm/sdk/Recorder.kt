3d8d4c980cc1015e51212efe317049f407341681
Arnold Noronha <arnold@tdrhq.com>
2020-10-01 10:45:00 -0700

    Initial look at the directory
    Summary:
    ...
    
    ...
    
    Differential Revision: https://phabricator.tdrhq.com/D2433

--- lib/src/main/kotlin/io/silkwrm/sdk/Recorder.kt
+++ lib/src/main/kotlin/io/silkwrm/sdk/Recorder.kt
@@ -2,19 +2,34 @@ package io.silkwrm.sdk
 
 import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.Options
+import java.io.File
 
 class Recorder {
+
+    fun getAllImages(dir: String) {
+        var dirFile = File(dir)
+        for (file in dirFile.listFiles()) {
+            System.out.println("got file: " + file)
+        }
+    }
+    private fun run(args: Array<String>) {
+        var options = Options()
+        options.addOption("d", "dir", true, "Directory with screenshots")
+        var parser = DefaultParser()
+        var cli = parser.parse(options, args)
+
+        var dir = cli.getOptionValue('d')
+
+        getAllImages(dir)
+        System.out.println("hello " + dir)
+    }
+
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            var options = Options()
-            options.addOption("d",  "dir",true, "Directory with screenshots")
-            var parser = DefaultParser()
-            var cli = parser.parse(options, args)
-
-            var dir = cli.getOptionValue('d')
-
-            System.out.println("hello " + dir)
+            Recorder().run(args)
         }
+
+
     }
 }
