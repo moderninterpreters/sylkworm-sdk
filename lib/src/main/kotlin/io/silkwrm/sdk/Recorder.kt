c3c739c44b8f6d75ac11073c834203f5df8ff460
Arnold Noronha <arnold@tdrhq.com>
2020-10-01 10:06:37 -0700

    ...

--- lib/src/main/kotlin/io/silkwrm/sdk/Recorder.kt
+++ lib/src/main/kotlin/io/silkwrm/sdk/Recorder.kt
@@ -1,5 +1,10 @@
 package io.silkwrm.sdk
 
 class Recorder {
-
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            System.out.println("hello")
+        }
+    }
 }
